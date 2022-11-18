46923fd33289544dac1c6dbfe523ed1d23d0e0be
andreihes <andrei.hes@ya.ru>
2022-11-18 18:59:47 +0400

    add ast bone

---
+++ lwjs/bone.py
@@ -0,0 +1,26 @@
+class Dot:
+  pass
+
+class Pin(Dot, list[Dot]):
+  pass
+
+class Arg(Dot, list[Dot]):
+  pass
+
+class Quo(Dot, list[Dot]):
+  pass
+
+class Kit(Dot, list[Pin|Arg|Quo]):
+  pass
+
+class Raw(Dot):
+  def __init__(self, raw: str):
+    self.Raw: str = raw
+
+class Sub(Dot):
+  def __init__(self, sub: Kit):
+    self.Sub: Kit = sub
+
+class Fun(Dot):
+  def __init__(self, fun: Kit):
+    self.Fun: Kit = fun
