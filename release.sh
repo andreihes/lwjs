3d47d8fcd2f3b789de8155c73d142a6795f8c3ad
andreihes <andrei.hes@ya.ru>
2022-12-07 14:48:31 +0400

    del sh

--- release.sh
+++
@@ -1,9 +0,0 @@
-  - name: Build package
-      run: python -m build
-    - name: Publish package
-      run: |
-        (
-          echo __token__
-          echo ${{ secrets.PYPI_API_TOKEN }}
-        ) | python -m twine upload --repository testpypi dist/*
-        
